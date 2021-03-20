a1c1b2e10de65229555741ab5b2ca738a99baa61
Josh Gaunt <joshgaunt04@gmail.com>
2021-03-20 15:12:53 +0200

    Change PolymerTemplate views to VerticalLayout
    Also removed template body code.

--- src/main/java/code/ndingujosh/igrama/views/enter/EnterView.java
+++ src/main/java/code/ndingujosh/igrama/views/enter/EnterView.java
@@ -1,35 +1,18 @@
 package code.ndingujosh.igrama.views.enter;
 
-import com.vaadin.flow.component.notification.Notification;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.polymertemplate.Id;
-import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
-import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.templatemodel.TemplateModel;
-import com.vaadin.flow.router.Route;
-import com.vaadin.flow.router.PageTitle;
 import code.ndingujosh.igrama.views.main.MainView;
-import com.vaadin.flow.router.RouteAlias;
-import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.Tag;
-import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
 
 @Route(value = "enter", layout = MainView.class)
 @RouteAlias(value = "", layout = MainView.class)
-@PageTitle("Enter an noun")
+@PageTitle("Igrama | Enter a noun")
 @CssImport("./views/enter/enter-view.css")
 @Tag("enter-view")
-public class EnterView extends PolymerTemplate<TemplateModel> {
+public class EnterView extends VerticalLayout {
 
-    @Id
-    private TextField name;
-
-    @Id
-    private Button sayHello;
-
-    public EnterView() {
-        sayHello.addClickListener(e -> {
-            Notification.show("Hello " + name.getValue());
-        });
-    }
 }
