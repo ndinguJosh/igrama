b72ec9d16e9d5cd1885494b5b74321f897091a4d
Josh Gaunt <joshgaunt04@gmail.com>
2021-03-21 10:03:53 +0200

    Define @OneToMany relationships between noun and nounClass

---
+++ src/main/java/code/ndingujosh/igrama/data/entity/Noun.java
@@ -0,0 +1,47 @@
+package code.ndingujosh.igrama.data.entity;
+
+import code.ndingujosh.igrama.data.AbstractEntity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "nouns")
+public class Noun extends AbstractEntity {
+    @NotNull
+    @NotEmpty
+    private String prefix;
+
+    @NotNull
+    @NotEmpty
+    private String root;
+
+    @ManyToOne
+    @JoinColumn(name = "class_id", nullable = false)
+    private NounClass nounClass;
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public void setPrefix(String prefix) {
+        this.prefix = prefix;
+    }
+
+    public String getRoot() {
+        return root;
+    }
+
+    public void setRoot(String root) {
+        this.root = root;
+    }
+
+    public NounClass getNounClass() {
+        return nounClass;
+    }
+
+    public void setNounClass(NounClass nounClass) {
+        this.nounClass = nounClass;
+    }
+}
--- src/main/java/code/ndingujosh/igrama/data/entity/NounClass.java
+++ src/main/java/code/ndingujosh/igrama/data/entity/NounClass.java
@@ -4,11 +4,14 @@ import code.ndingujosh.igrama.data.AbstractEntity;
 
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.util.Set;
 
 @Entity
+@Table(name="classes")
 public class NounClass extends AbstractEntity {
     @NotNull
     @NotEmpty
@@ -69,6 +72,9 @@ public class NounClass extends AbstractEntity {
     @NotNull
     @NotEmpty
     private String enumerative;
+
+    @OneToMany(mappedBy = "nounClass")
+    private Set<Noun> nouns;
 
     private boolean isPlural;
 
@@ -198,5 +204,13 @@ public class NounClass extends AbstractEntity {
 
     public void setPlural(boolean plural) {
         isPlural = plural;
+    }
+
+    public Set<Noun> getNouns() {
+        return nouns;
+    }
+
+    public void setNouns(Set<Noun> nouns) {
+        this.nouns = nouns;
     }
 }
